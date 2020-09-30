48a8643b725d23c5123c756d1720fcc28345ac30
peter fors <peter.fors@mindkiller.com>
2020-09-30 09:53:13 +0200

    Now works correctly with sending Options to the telnet client.

--- main.c
+++ main.c
@@ -27,15 +27,6 @@ enum {
 #define LF 10
 #define NUL 0
 
-uint64_t telnet_state;
-
-static DWORD receive_cmds(LPVOID lpParam) {
-	SOCKET current_client = (SOCKET)lpParam;
-
-	char buf[100];				// buffer to hold our recived data
-	char sendData[100];		// buffer to hold our sent data
-	int res;						// for error checking
-
 
 #define IAC     255                    /* interpret as command: */
 #define DONT    254                    /* you are not to use option */
@@ -59,41 +50,105 @@ static DWORD receive_cmds(LPVOID lpParam) {
 #define SUSP    237                    /* Suspend process */
 #define xEOF    236                    /* End of file: EOF is already used... */
 
+/* telnet options */
+#define TELOPT_BINARY			0			/* 8-bit data path */
+#define TELOPT_ECHO				1			/* echo */
+#define TELOPT_RCP				2			/* prepare to reconnect */
+#define TELOPT_SGA				3			/* suppress go ahead */
+#define TELOPT_NAMS				4			/* approximate message size */
+#define TELOPT_STATUS			5			/* give status */
+#define TELOPT_TM					6			/* timing mark */
+#define TELOPT_RCTE				7			/* remote controlled transmission and echo */
+#define TELOPT_NAOL			 	8			/* negotiate about output line width */
+#define TELOPT_NAOP			 	9			/* negotiate about output page size */
+#define TELOPT_NAOCRD			10			/* negotiate about CR disposition */
+#define TELOPT_NAOHTS			11			/* negotiate about horizontal tabstops */
+#define TELOPT_NAOHTD			12			/* negotiate about horizontal tab disposition */
+#define TELOPT_NAOFFD			13			/* negotiate about formfeed disposition */
+#define TELOPT_NAOVTS			14			/* negotiate about vertical tab stops */
+#define TELOPT_NAOVTD			15			/* negotiate about vertical tab disposition */
+#define TELOPT_NAOLFD			16			/* negotiate about output LF disposition */
+#define TELOPT_XASCII			17			/* extended ascic character set */
+#define TELOPT_LOGOUT			18			/* force logout */
+#define TELOPT_BM					19			/* byte macro */
+#define TELOPT_DET				20			/* data entry terminal */
+#define TELOPT_SUPDUP			21			/* supdup protocol */
+#define TELOPT_SUPDUPOUTPUT	22			/* supdup output */
+#define TELOPT_SNDLOC			23			/* send location */
+#define TELOPT_TTYPE				24			/* terminal type */
+#define TELOPT_EOR				25			/* end or record */
+#define TELOPT_TUID				26			/* TACACS user identification */
+#define TELOPT_OUTMRK			27			/* output marking */
+#define TELOPT_TTYLOC			28			/* terminal location number */
+#define TELOPT_3270REGIME		29			/* 3270 regime */
+#define TELOPT_X3PAD				30			/* X.3 PAD */
+#define TELOPT_NAWS				31			/* window size */
+#define TELOPT_TSPEED			32			/* terminal speed */
+#define TELOPT_LFLOW				33			/* remote flow control */
+#define TELOPT_LINEMODE			34			/* Linemode option */
+#define TELOPT_XDISPLOC			35			/* X Display Location */
+#define TELOPT_OLD_ENVIRON		36			/* Old - Environment variables */
+#define TELOPT_AUTHENTICATION 37			/* Authenticate */
+#define TELOPT_ENCRYPT			38			/* Encryption option */
+#define TELOPT_NEW_ENVIRON		39			/* New - Environment variables */
+#define TELOPT_EXOPL				255		/* extended-options-list */
 
-#define TELNET_WILL	251
-#define TELNET_WONT	252
-#define TELNET_DO		253
-#define TELNET_DONT	254
-#define IAC				255
+#define	NTELOPTS	(1+TELOPT_NEW_ENVIRON)
+char *telopts[NTELOPTS+1] = {
+	"BINARY", "ECHO", "RCP", "SUPPRESS GO AHEAD", "NAME",
+	"STATUS", "TIMING MARK", "RCTE", "NAOL", "NAOP",
+	"NAOCRD", "NAOHTS", "NAOHTD", "NAOFFD", "NAOVTS",
+	"NAOVTD", "NAOLFD", "EXTEND ASCII", "LOGOUT", "BYTE MACRO",
+	"DATA ENTRY TERMINAL", "SUPDUP", "SUPDUP OUTPUT",
+	"SEND LOCATION", "TERMINAL TYPE", "END OF RECORD",
+	"TACACS UID", "OUTPUT MARKING", "TTYLOC",
+	"3270 REGIME", "X.3 PAD", "NAWS", "TSPEED", "LFLOW",
+	"LINEMODE", "XDISPLOC", "OLD-ENVIRON", "AUTHENTICATION",
+	"ENCRYPT", "NEW-ENVIRON",
+	0
+};
 
-#define SB				250
 #define TTYPE			24
 #define SEND			1
-#define SE				240
 
-	char wont_linemode[] = {
-		IAC, TELNET_DONT, 34,
+uint64_t telnet_state;
 
+
+
+static void send_option(SOCKET sock, int command, int option) {
+	unsigned char b[3];
+
+	b[0] = IAC;
+	b[1] = command;
+	b[2] = option;
+	send(sock, b, 3, 0);
+}
+
+static DWORD receive_cmds(LPVOID lpParam) {
+	SOCKET sock = (SOCKET)lpParam;
+
+	char buf[100];				// buffer to hold our recived data
+	//char sendData[100];		// buffer to hold our sent data
+	int res;						// for error checking
+
+	char ask_ttype[] = {
 		IAC,SB,TTYPE,SEND,IAC,SE,
-
-		//IAC, WILL_USE, 1,
-		//255, 252, 34,
-		//255, 254, 34,
-		//255, 251, 1,
-
 	};
 
-
-	send(current_client, wont_linemode, sizeof(wont_linemode), 0);
+	send_option(sock, WILL, TELOPT_ECHO);
+	send_option(sock, WILL, TELOPT_SGA);
+	send_option(sock, WONT, TELOPT_LINEMODE);
+	send(sock, ask_ttype, sizeof(ask_ttype), 0);
 
 	while(1) {
-		res = recv(current_client, buf, 1, 0); // recv cmds
+		res = recv(sock, buf, 1, 0); // recv cmds
+
+		if(res == 0) break;	// disconnect
 
 		int c = (unsigned char)*buf;
 
 		switch(telnet_state) {
-			case TOP_LEVEL: //{		// fall through, for now
-//			} break;
+			case TOP_LEVEL:			// fall through, for now
 			case SEEN_CR: {
 				if((c == 0) || (c == '\n') && telnet_state == SEEN_CR) {
 					telnet_state = TOP_LEVEL;
@@ -125,23 +180,23 @@ static DWORD receive_cmds(LPVOID lpParam) {
 				}
 			} break;
 			case SEEN_WILL: {
-				printf("WILL\n");
+				printf("WILL %s\n", telopts[c]);
 				telnet_state = TOP_LEVEL;
 			} break;
 			case SEEN_WONT: {
-				printf("WONT\n");
+				printf("WONT %s\n", telopts[c]);
 				telnet_state = TOP_LEVEL;
 			} break;
 			case SEEN_DO: {
-				printf("DO\n");
+				printf("DO %s\n", telopts[c]);
 				telnet_state = TOP_LEVEL;
 			} break;
 			case SEEN_DONT: {
-				printf("DONT\n");
+				printf("DONT %s\n", telopts[c]);
 				telnet_state = TOP_LEVEL;
 			} break;
 			case SEEN_SB: {
-				printf("SB\n");
+				printf("SB %s\n", telopts[c]);
 				telnet_state = TOP_LEVEL;
 			} break;
 			case SUBNEG_OT: {
@@ -154,6 +209,7 @@ static DWORD receive_cmds(LPVOID lpParam) {
 			} break;
 		}
 	}
+	return 0;
 }
 
 int main() {
